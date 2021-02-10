4e32c61f59212c709d7ca8747e1001c93a0c3af7
kenorb <kenorb@users.noreply.github.com>
2021-02-10 00:34:00 +0000

    Optimize Indicator params (pf=1.48; dd=3.14; tt=229)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -6,11 +6,11 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_TMA_True_Params_M5 : Indi_TMA_True_Params {
   Indi_TMA_True_Params_M5() : Indi_TMA_True_Params(indi_tmat_defaults, PERIOD_M5) {
-    atr_multiplier = 1.0;
-    atr_period = 16.0;
+    atr_multiplier = 3.0;
+    atr_period = 14;
     atr_tf = 0.0;
     bars_to_process = 4.0;
-    half_length = 3.0;
+    half_length = 2.0;
     shift = 0;
   }
 } indi_tmat_m5;
@@ -22,7 +22,7 @@ struct Stg_TMA_True_Params_M5 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
